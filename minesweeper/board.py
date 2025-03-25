2e89e874df33398fd8d68ef9a41ac85cd0876fe6
nyxx <leila.wilde@laplateforme.io>
2025-03-25 21:07:15 +0000

    unduckified code

--- minesweeper/board.py
+++ minesweeper/board.py
@@ -1,6 +1,6 @@
 from random import random 
 
-class Duckfield():
+class Board():
     def __init__(self, difficulty):
         self.difficulty = difficulty
         if difficulty == 'beginner':
@@ -14,19 +14,19 @@ class Duckfield():
         for _ in range(self.size):
             row = []
             for _ in range(self.size):
-                row.append({'hidden': True, 'duck': False})
+                row.append({'hidden': True, 'mine': False})
             self.grid.append(row)
                 
     def reveal(self, x, y):
         self.grid[x][y]['hidden'] = False 
-        return self.grid[x][y]['duck']
+        return self.grid[x][y]['mine']
     
-    def add_ducks(self):
-        ducks_required = round((self.size ** 2) * 0.2)
-        ducks_added = 0
-        while ducks_added < ducks_required:
+    def add_mines(self):
+        mines_required = round((self.size ** 2) * 0.2)
+        mines_added = 0
+        while mines_added < mines_required:
             x = round(random()*(self.size -1))
             y = round(random()*(self.size -1))
-            if not self.grid[x][y]['duck']:
-                self.grid[x][y]['duck'] = True
-                ducks_added += 1
+            if not self.grid[x][y]['mine']:
+                self.grid[x][y]['mine'] = True
+                mines_added += 1
--- minesweeper/test_board.py
+++ minesweeper/test_board.py
@@ -1,45 +1,45 @@
-from board import Duckfield
+from board import Board
 
-def test_create_duckfield():
-    duckfield = Duckfield('beginner')
-    assert duckfield.difficulty == 'beginner'
-    assert duckfield.size == 9
-    assert len(duckfield.grid) == 9
-    assert len(duckfield.grid[0]) == 9
+def test_create_board():
+    board = Board('beginner')
+    assert board.difficulty == 'beginner'
+    assert board.size == 9
+    assert len(board.grid) == 9
+    assert len(board.grid[0]) == 9
     
-    duckfield = Duckfield('intermediate')
-    assert duckfield.difficulty == 'intermediate'
-    assert duckfield.size == 16
-    assert len(duckfield.grid) == 16
-    assert len(duckfield.grid[0]) == 16
+    board = Board('intermediate')
+    assert board.difficulty == 'intermediate'
+    assert board.size == 16
+    assert len(board.grid) == 16
+    assert len(board.grid[0]) == 16
     
-    duckfield = Duckfield('expert')
-    assert duckfield.difficulty == 'expert'
-    assert duckfield.size == 24
-    assert len(duckfield.grid) == 24 
-    assert len(duckfield.grid[0]) == 24       
+    board = Board('expert')
+    assert board.difficulty == 'expert'
+    assert board.size == 24
+    assert len(board.grid) == 24 
+    assert len(board.grid[0]) == 24       
     
-test_create_duckfield()
+test_create_board()
 
 def test_reveal():
-    duckfield = Duckfield('beginner')
-    for row in duckfield.grid:
+    board = Board('beginner')
+    for row in board.grid:
         for tile in row:
             assert tile['hidden']
-    is_duck = duckfield.reveal(0, 0)
-    assert is_duck == True or is_duck == False
-    assert duckfield.grid[0][0]['hidden'] == False
+    is_mine = board.reveal(0, 0)
+    assert is_mine == True or is_mine == False
+    assert board.grid[0][0]['hidden'] == False
 
 test_reveal()
 
-def test_percentage_ducks():
-    duckfield = Duckfield('beginner')
-    duckfield.add_ducks()
-    duck_count = 0
-    for row in duckfield.grid:
-        for tile in row:
-            if tile['duck']:
-                duck_count += 1
-    assert duck_count == round((duckfield.size ** 2) * 0.2)
+def test_percentage_mines():
+    board = Board('beginner')
+    board.add_mines()
+    mine_count = 0
+    for row in board.grid:
+        for cell in row:
+            if cell['mine']:
+                mine_count += 1
+    assert mine_count == round((board.size ** 2) * 0.2)
 
-test_percentage_ducks()
+test_percentage_mines()
