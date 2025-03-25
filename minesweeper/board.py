d73398874a8fe24f666f69e9de55058b8fb57358
nyxx <leila.wilde@laplateforme.io>
2025-03-25 21:34:18 +0000

    first click logic added

--- minesweeper/board.py
+++ minesweeper/board.py
@@ -18,7 +18,14 @@ class Board():
             self.grid.append(row)
                 
     def reveal(self, x, y):
+        all_hidden = True
+        for row in self.grid:
+            for cell in row:
+                if cell.hidden == False:
+                    all_hidden = False
         self.grid[x][y].hidden = False 
+        if all_hidden:
+            self.add_mines()
         return self.grid[x][y].mine
     
     def add_mines(self):
@@ -27,7 +34,7 @@ class Board():
         while mines_added < mines_required:
             x = round(randint(0, self.size -1))
             y = round(randint(0, self.size -1))
-            if not self.grid[x][y].mine:
+            if (not self.grid[x][y].mine) and self.grid[x][y].hidden:
                 self.grid[x][y].mine = True
                 mines_added += 1
                 
