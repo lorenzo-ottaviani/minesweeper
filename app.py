2b9046c18a37e30057d615ab6b52056ca631d730
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-03-26 10:00:47 +0100

    feat: add first version of the welcome scene in GUI

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
-import customtkinter as ctk
+"""
+Authors : Lorenzo OTTAVIANI, Morgane ROSSI et Leila WILDE.
+Date : 26/03/2025 09h43
+Aim of the program :
+    Execute a minesweeper game using Custom Tkinter.
+Inputs : ∅
+Output : Minesweeper classical game.
+"""
+
 from models import *
 
 
@@ -8,7 +16,7 @@ class App:
     def __init__(self):
         """"""
         self.controller = GUI()
-
+ 
     def run(self):
         """"""
         self.controller.mainloop()
--- models/GUI.py
+++ models/GUI.py
@@ -5,28 +5,104 @@ class GUI(ctk.CTk):
     """ Graphical User Interface (GUI) of the program. """
 
     def __init__(self):
-        """"""
         super().__init__()
 
         self.title("Minesweeper")
-        self.geometry("1200x1000")
+        self.geometry("1080x900")  # Total window size
 
         self.configure(fg_color="#99d5ff")
 
-        self.grid_columnconfigure((0, 2), weight=1)
+        # Configure rows and columns
         self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure((0, 1, 2), weight=1)
 
         self.draw_welcome_scene()
 
-        # Test of a board container shape
-        # self.board = ctk.CTkFrame(self, width=1000, height=1000)
-        # self.board.place(relx=0.5, rely=0.5, anchor="center")
-
     def draw_welcome_scene(self):
-        """"""
-        easy_frame = ctk.CTkFrame(self, width=360, height=700)
-        easy_frame.grid(row=0, column=0, padx=10)
-        medium_frame = ctk.CTkFrame(self, width=360, height=700)
-        medium_frame.grid(row=0, column=1, padx=10)
-        hard_frame = ctk.CTkFrame(self, width=360, height=700)
-        hard_frame.grid(row=0, column=2, padx=10)
+        """
+        Draw the welcome scene screen.
+        return : ∅
+        """
+
+        def draw_beginner_frame():
+            """
+            Draw the beginner frame.
+            return : ∅
+            """
+            beginner_frame = ctk.CTkFrame(self, width=360, height=900)
+            beginner_frame.grid(row=0, column=0, padx=10, pady=10)
+
+            beginner_frame.grid_rowconfigure((0, 1, 2, 3), weight=1)
+            beginner_frame.grid_columnconfigure(0, weight=1)
+
+            beginner_title = ctk.CTkLabel(beginner_frame, text="Beginner", font=("Arial", 24), text_color="blue")
+            beginner_title.grid(row=0, column=1, padx=5, pady=5)
+
+            beginner_shape = ctk.CTkLabel(beginner_frame, text="9 x 9", font=("Arial", 18))
+            beginner_shape.grid(row=1, column=1, padx=5, pady=5)
+
+            beginner_mines = ctk.CTkLabel(beginner_frame, text=" ~ 10 mines", font=("Arial", 18))
+            beginner_mines.grid(row=2, column=1, padx=5, pady=5)
+
+            beginner_button = ctk.CTkButton(beginner_frame, text="Play", command=self.start_beginner_game)
+            beginner_button.grid(row=3, column=1, padx=5, pady=5)
+
+        def draw_intermediate_frame():
+            """
+            Draw the intermediate frame.
+            return : ∅
+            """
+        intermediate_frame = ctk.CTkFrame(self, width=360, height=900)
+        intermediate_frame.grid(row=0, column=1, padx=10, pady=10)
+
+        intermediate_frame.grid_rowconfigure((0, 1, 2, 3), weight=1)
+        intermediate_frame.grid_columnconfigure(0, weight=1)
+
+        intermediate_title = ctk.CTkLabel(intermediate_frame, text="Intermediate", font=("Arial", 24),
+                                          text_color="green")
+        intermediate_title.grid(row=0, column=1, padx=5, pady=5)
+
+        intermediate_shape = ctk.CTkLabel(intermediate_frame, text="16 x 16", font=("Arial", 18))
+        intermediate_shape.grid(row=1, column=1, padx=5, pady=5)
+
+        intermediate_mines = ctk.CTkLabel(intermediate_frame, text=" ~ 40 mines", font=("Arial", 18))
+        intermediate_mines.grid(row=2, column=1, padx=5, pady=5)
+
+        intermediate_button = ctk.CTkButton(intermediate_frame, text="Play", command=self.start_intermediate_game)
+        intermediate_button.grid(row=3, column=1, padx=5, pady=5)
+
+        def draw_expert_frame():
+            """
+            Draw the expert frame.
+            return : ∅
+            """
+            expert_frame = ctk.CTkFrame(self, width=360, height=900)
+            expert_frame.grid(row=0, column=2, padx=10, pady=10)
+
+            expert_frame.grid_rowconfigure((0, 1, 2, 3), weight=1)
+            expert_frame.grid_columnconfigure(0, weight=1)
+
+            expert_title = ctk.CTkLabel(expert_frame, text="Expert", font=("Arial", 24), text_color="red")
+            expert_title.grid(row=0, column=1, padx=5, pady=5)
+
+            expert_shape = ctk.CTkLabel(expert_frame, text="24 x 24", font=("Arial", 18))
+            expert_shape.grid(row=1, column=1, padx=5, pady=5)
+
+            expert_mines = ctk.CTkLabel(expert_frame, text=" ~ 99 mines", font=("Arial", 18))
+            expert_mines.grid(row=2, column=1, padx=5, pady=5)
+
+            expert_button = ctk.CTkButton(expert_frame, text="Play", command=self.start_expert_game)
+            expert_button.grid(row=3, column=1, padx=5, pady=5)
+
+        draw_beginner_frame()
+        draw_intermediate_frame()
+        draw_expert_frame()
+
+    def start_beginner_game(self):
+        pass
+
+    def start_intermediate_game(self):
+        pass
+
+    def start_expert_game(self):
+        pass
