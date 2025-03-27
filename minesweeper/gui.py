68d8bcd762621cdbc310249f3a66aa69e0d98ad1
nyxx <leila.wilde@laplateforme.io>
2025-03-27 16:39:20 +0000

    created pseudocode for a gui class

---
+++ minesweeper/gui.py
@@ -0,0 +1,32 @@
+from board import Board
+from game import Game 
+from timer import Timer
+import customtkinter as ctk
+
+class GUI(ctk.CTk):
+    def __init__(self):
+        pass
+    
+    def welcome_screen(self):
+        pass
+    
+    def start_game(self):
+        pass
+    
+    def display_board(self):
+        pass
+    
+    def manage_left_click(self):
+        pass
+    
+    def manage_right_click(self):
+        pass
+    
+    def update_buttons(self):
+        pass
+    
+    def reveal_mines(self):
+        pass
+    
+    def restart(self):
+        pass  
