cb5f7e2bf1fe3956ad446c475102a5799dbb81d4
nyxx <leila.wilde@laplateforme.io>
2025-03-27 13:41:46 +0000

    added win logic

--- minesweeper/game.py
+++ minesweeper/game.py
@@ -1,4 +1,5 @@
 from board import Board, Cell
+import time
 
 class Game:
     def __init__(self, difficulty):
@@ -8,10 +9,18 @@ class Game:
         self.timer = None
     
     def start_game(self):
+        self.game_over = False
         pass
         
     def check_win(self):
-        pass
+        '''
+        Player wins when all the cells except the mines are revealed
+        '''
+        for row in self.board.grid:
+            for cell in row:
+                if not cell.mine and cell.hidden:
+                    return False
 
     def end_game(self):
+        self.game_over = True
         pass
