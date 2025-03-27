1153da00d45140749379d2453c33d7de6805c481
nyxx <leila.wilde@laplateforme.io>
2025-03-27 16:22:07 +0000

    debugged game unit test

--- minesweeper/test_game.py
+++ minesweeper/test_game.py
@@ -3,19 +3,19 @@ import unittest
 
 class TestGame(unittest.TestCase):
     def test_start_game(self):
-        game = Game(difficulty=3)
+        game = Game(difficulty='beginner')
         game.start_game()
         assert not game.game_over, 'game should not be over after start_game'
         assert game.start_game is not None, 'time should be set after start_game'
     
     def test_check_win(self):
-        game = Game(difficulty=3)
+        game = Game(difficulty='beginner')
         game.start_game()
         game.check_win()
         
     def test_end_game(self):
-        game = Game(difficulty=3)
-        game.start()
+        game = Game(difficulty='beginner')
+        game.start_game()
         game.end_game()
         assert game.game_over, 'Game shoud be over after game_over'
         assert game.time_passed > 0, 'time passed should be greater than 0 after end_game'
