45fe159e49e4bdca55f664512fb1d22a92952267
nyxx <leila.wilde@laplateforme.io>
2025-03-27 12:06:25 +0000

    created files for timer class

---
+++ minesweeper/test_timer.py
@@ -0,0 +1,11 @@
+from timer import Timer
+import unittest
+
+class TestTimer(unittest.TestCase):
+    def test_timer():
+        pass
+
+
+
+if __name__=='__main__':
+    unittest.main()
---
+++ minesweeper/timer.py
@@ -0,0 +1,4 @@
+class Timer:
+    def __init__(self):
+        self.running = False
+        self.start_timer = None
