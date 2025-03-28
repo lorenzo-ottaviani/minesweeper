5f1efa462cb962a2b04e174e87d07a33a8b9cd16
Lorenzo OTTAVIANI <lorenzo.ottaviani@laplateforme.io>
2025-03-28 09:10:11 +0100

    refactor: update draw board screen structure

--- models/board_screen.py
+++ models/board_screen.py
@@ -14,10 +14,15 @@ class BoardScreen(ctk.CTkFrame):
         self.grid_columnconfigure(0, weight=1)
         self.configure(fg_color="white")
 
+        self.cells = []
+
         self.draw_board_screen()
 
     def draw_board_screen(self):
-        """"""
+        """
+        Draw the board screen.
+        :return: ∅
+        """
 
         def generate_mines(cell_range, number_of_mines):
             """"""
@@ -27,39 +32,56 @@ class BoardScreen(ctk.CTkFrame):
                 mines.add(mine)
             return mines
 
-        def cell_left_click_action(event, row, col):
-            """"""
-            button = cells[row][col]
-            current_text = button.cget("text")
-
-            if current_text == "":
-                button.configure(text="🏴", font=("Arial", 18))
-            elif current_text == "🏴":
-                button.configure(text="🦆", font=("Arial", 18))
-            elif current_text == "🦆":
-                button.configure(text="", font=("Arial", 18))
-
-        def cell_right_click_action(event, row, col):
-            button = cells[row][col]
-            current_text = button.cget("text")
-            if current_text == "💣":
-                button.configure(text="🚩", font=("Arial", 18))
-            elif current_text == "🚩":
-                button.configure(text="💣", font=("Arial", 18))
-
         board_frame = ctk.CTkFrame(self, width=360, height=900, bg_color="white")
         board_frame.grid(row=0, column=1)
 
         mines = generate_mines(self.cell_range, randint(*self.number_of_mines))
 
-        cells = []
         for row in range(self.cell_range):
             cells_row = []
             for col in range(self.cell_range):
                 cell = ctk.CTkButton(board_frame, text="", width=self.cell_size, height=self.cell_size)
-                cell.bind("<Button-1>", lambda event, r=row, c=col: cell_left_click_action(event, r, c))
-                cell.bind("<Button-3>", lambda event, r=row, c=col: cell_right_click_action(event, r, c))
+                cell.bind("<Button-1>", lambda event, r=row, c=col: self.cell_left_click_action(event, r, c))
+                cell.bind("<Button-3>", lambda event, r=row, c=col: self.cell_right_click_action(event, r, c))
 
                 cell.grid(row=row, column=col, padx=0.5, pady=0.5)
                 cells_row.append(cell)
-            cells.append(cells_row)
+            self.cells.append(cells_row)
+
+    def cell_left_click_action(self, event, row, col):
+        """
+        Call of the cell left click action.
+        :param event: The button click event.
+        :param row: The board rows.
+        :param col: The board columns.
+        :return: ∅
+        """
+        button = self.cells[row][col]
+        current_text = button.cget("text")
+
+        if current_text == "":
+            button.configure(text="💣", font=("Arial", 18))
+        elif current_text == "💣":
+            button.configure(text="🦆", font=("Arial", 18))
+        elif current_text == "🦆":
+            button.configure(text="", font=("Arial", 18))
+
+    def cell_right_click_action(self, event, row, col):
+        """
+        Call of the cell right click action.
+        :param event: The button click event.
+        :param row: The board rows.
+        :param col: The board columns.
+        :return: ∅
+        """
+        button = self.cells[row][col]
+        current_text = button.cget("text")
+
+        icons = ["🏴", "💣", "🦆", "🚩", "❓"]
+
+        if current_text == "":
+            button.configure(text="🏴", font=("Arial", 18))
+        elif current_text == "🏴":
+            button.configure(text="❓", font=("Arial", 18))
+        elif current_text == "❓":
+            button.configure(text="", font=("Arial", 18))
